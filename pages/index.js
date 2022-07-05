41e35c3b1613e47eb6568eec451ae429700d7247
Ram Murmu <runashInc@outlook.com>
2022-07-05 23:20:54 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
         />
         <div className={styles.description}>{' '}<code className={styles.code}><a href="https://runash.live/schedule">📺Go live</a></code></div>
         <h1 className={styles.title}> 
-          Next gen<br /> & future of <a href="https://">eCommerce
+          Next gen<br /> & future of <a href="https://">eCommerce</a>
         </h1>
         <p className={styles.description}>
             Build engaging interactive<br /> real life experience</p> 
