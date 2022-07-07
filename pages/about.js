a00423f161d938270550d143413126d0d4abb42c
Ram Murmu <runashInc@outlook.com>
2022-07-07 15:01:03 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -6,8 +6,9 @@ export default function About() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>About</h1>
+
         <div className={styles.description}>{' '}<code className={styles.code}>👏 Welcome to RunAsh.live</code></div>
-        <h5 className={styles.description}>Vision:Enabling and accessing face to face,self made and real experience to access physical reality</h5>
+        <h5 className={styles.description}>Mission:Enabling and accessing face to face,self made and real experience to access physical reality</h5>
         <div className={styles.grid}>
          <div className={styles.description}>
          <h2>Oneliner</h2>
@@ -15,8 +16,8 @@ export default function About() {
         </div>
 
         <div className={styles.description}>
-         <h2>Discipline and culture</h2>
-         <p>Discipline and learning is the backbone of the development of startup journey & culture is our behavior</p>
+         <h2>Discipline </h2>
+         <p>Discipline and learning is the backbone of the development of our startup journey</p>
         </div>
 
         <div className={styles.description}>
@@ -32,16 +33,16 @@ export default function About() {
        
        <div className={styles.description}>
        <h2>Executive Profiles</h2>
-       <p>The single most important element of our business model is the management team responsible for making the live retail streaming platform. immediate market-specific knowledge, and experience in implementing the business plans. & changing the nature of retailing & redefine the business mode</p>
+       <p>The single most important element of our business model is the management team responsible for making the live retail streaming platform.</p>
        </div>
 
-       </div>
+       
 
        <p className={styles.description}>Backed by{' '}<code className={styles.code}>Open Source </code>Supporters</p>
-       <>
+       
        <p><a href="https://" title="title">Licence: MIT GitHub</a></p>
        
-       </>
+       
 
       
 
