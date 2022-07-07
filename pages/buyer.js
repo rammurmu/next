a3ce3a0e59efac7a62d07f77d1b919fcf5c976e4
Ram Murmu <runashInc@outlook.com>
2022-07-07 19:10:31 +0530

    Update buyer.js

--- pages/buyer.js
+++ pages/buyer.js
@@ -18,16 +18,16 @@ export default function Buyer() {
 
           <p className={styles.code}>All Categories</p>
           <code className={styles.code}>Short by</code>
-          <code className={styles.code}>🍎Fruit</code><br /><code className={styles.code}>🥦Vegetable</code>{' '}<code className={styles.code}>💐Flower</code>{' '}<code className={styles.code}>🥫Grocery</code>{' '}
+          <div className={styles.code}>🍎Fruit</code><br /><code className={styles.code}>🥦Vegetable</code>{' '}<code className={styles.code}>💐Flower</code>{' '}<code className={styles.code}>🥫Grocery</code></div>
           <div className={styles.grid}>
           <a href="https://" className={styles.card}>
             <h2>📺Stream Store &rarr;</h2>
-            <p>Watch Now</p>
+            <p><code>Watch Now</code></p>
           </a>
 
           <a 
             href="https://" className={styles.card}>
-            <h2>📺Strem Store &rarr;</h2>
+            <h2>📺Stream Store &rarr;</h2>
             <p>Watch Now </p>
           </a>
           </div>
