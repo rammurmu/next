79f7e2a32a167002fdefe80cbfd00a9be22ddb70
Ram Murmu <runashInc@outlook.com>
2022-07-07 15:05:23 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,14 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.cimport styles from '../styles/About.module.css'
 import Link from 'next/link'
+
 
 export default function About() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>About</h1>
-
         <div className={styles.description}>{' '}<code className={styles.code}>👏 Welcome to RunAsh.live</code></div>
-        <h5 className={styles.description}>Mission:Enabling and accessing face to face,self made and real experience to access physical reality</h5>
+        <h5 className={styles.description}>Vision:Enabling and accessing face to face,self made and real experience to access physical reality</h5>
         <div className={styles.grid}>
          <div className={styles.description}>
          <h2>Oneliner</h2>
@@ -16,8 +16,8 @@ export default function About() {
         </div>
 
         <div className={styles.description}>
-         <h2>Discipline </h2>
-         <p>Discipline and learning is the backbone of the development of our startup journey</p>
+         <h2>Discipline and culture</h2>
+         <p>Discipline and learning is the backbone of the development of startup journey & culture is our behavior</p>
         </div>
 
         <div className={styles.description}>
@@ -33,16 +33,16 @@ export default function About() {
        
        <div className={styles.description}>
        <h2>Executive Profiles</h2>
-       <p>The single most important element of our business model is the management team responsible for making the live retail streaming platform.</p>
+       <p>The single most important element of our business model is the management team responsible for making the live retail streaming platform. immediate market-specific knowledge, and experience in implementing the business plans. & changing the nature of retailing & redefine the business mode</p>
        </div>
 
-       
+       </div>
 
        <p className={styles.description}>Backed by{' '}<code className={styles.code}>Open Source </code>Supporters</p>
-       
+       <>
        <p><a href="https://" title="title">Licence: MIT GitHub</a></p>
        
-       
+       </>
 
       
 
