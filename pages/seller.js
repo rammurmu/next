1598923b23ae5f6a8f2dc3779cde0287801c9a94
Ram Murmu <runashInc@outlook.com>
2022-07-07 21:30:40 +0530

    Update seller.js

--- pages/seller.js
+++ pages/seller.js
@@ -5,9 +5,11 @@ export default function Seller() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>Seller</h1>
+        <h1>Seller</h1>   
         <div className={styles.description}><code className={styles.code}>👏 Welcome to runash</code></div>
-        <h2 className={styles.description}>
+        <div className={styles.title}>RunAsh live<br />streaming<br />for retailers</div>
+        <p>Give you the best real selling experience with all the features you need for building hybrid live streaming store, support,smart building,and more .no money needed</p>
+        <div className={styles.title}>
         Give your store a<br /> real experience <br />on a complete digitization</h2>
         <p>Schedule sell on real experience</p>
 
