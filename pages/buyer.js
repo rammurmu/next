798774837dab96f9474caceb5eb941b48b4e2475
Ram Murmu <runashInc@outlook.com>
2022-07-07 14:50:47 +0530

    Update buyer.js

--- pages/buyer.js
+++ pages/buyer.js
@@ -5,7 +5,7 @@ export default function Buyer() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <code className={styles.code}>👏 Welcome to RunAsh.live</code>
+        <code className={styles.code}>👏 Welcome to RunAsh</code>
         <h1 className={styles.description}>📺 Live watch<br />and Live on demand</h1>
 
         <div className={styles.description}>{' '}
@@ -13,13 +13,14 @@ export default function Buyer() {
 
         <h1 className={styles.title}>Whatched <a href="https://">Live stream</a></h1>
 
-          <h2 className={styles.description}>LIVE ON DEAMAND</h2>
+          <h2 className={styles.description}>Live on demand</h2>
 
-          <p className={styles.paragraph}>Whatching some previous live streaming,you can watch some live stream to receive notifications for new live streaming and updats.when the buyer updates the live streaming their organic products on daily basis,</p>
+          <p className={styles.paragraph}>Whatching some previous live streaming</p>
 
           <p className={styles.card}>All Categories</p>
+
           <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a href="https://" className={styles.card}>
             <h2>Fruit Stream &rarr;</h2>
             <p>Watched </p>
           </a>
@@ -29,7 +30,6 @@ export default function Buyer() {
             <h2>Vegetable Strem &rarr;</h2>
             <p>Watched </p>
           </a>
-
           </div>
 
 
