695125b658c6d3b526ad32736e871e9c44759427
Ram Murmu <runashInc@outlook.com>
2022-07-08 11:04:06 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import Layout from '../components/layout'
+
 
 export default function About() {
   return (
