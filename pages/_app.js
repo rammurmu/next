a63fd569c3665d8b14fe9c312df495fcd561f3c9
Ram Murmu <runashInc@outlook.com>
2022-07-08 11:24:09 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,24 @@
 import '../styles/globals.css'
+import Layout from '../components/layout'
 
 export default function MyApp({ Component, pageProps }) {
-const getLayout = Component.getLayout || ((page) => page)
-  return getLayout(<Component {...pageProps} />)
+
+  return (
+     <Layout>
+        <Component {...pageProps} />
+     </Layout>
+  ) 
 }
 
-export default MyApp
+// export default MyApp
 
-// export default function MyApp({ Component, pageProps }) { //
-  // Use the layout defined at the page level, if available //
-  // const getLayout = Component.getLayout || ((page) => page) //
 
- // return getLayout(<Component {...pageProps} />) //
+// import Layout from '../components/layout'
+
+// export default function MyApp({ Component, pageProps }) {
+ // return (
+   // <Layout>
+      // <Component {...pageProps} />
+    // </Layout>
+ // )
+// }
