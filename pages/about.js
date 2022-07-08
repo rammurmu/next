8a74afae11aa1e3363f52c069504ddd471856c43
Ram Murmu <runashInc@outlook.com>
2022-07-08 10:50:17 +0530

    Create about.js

--- pages/about.js
+++ pages/about.js
@@ -6,8 +6,8 @@ export default function About() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>About</h1>
-        <div className={styles.description}>{' '}<code className={styles.code}>👏 Welcome to RunAsh.live</code></div>
-        <h5 className={styles.description}>Vision:Enabling and accessing face to face,self made and real experience to access physical reality</h5>
+        <div className={styles.description}>{' '}<code className={styles.code}>👏 Welcome to RunAsh</code></div>
+        <h2 className={styles.description}>Vision:Enabling and accessing face to face,self made and real experience to access physical reality</h2>
         <div className={styles.grid}>
          <div className={styles.description}>
          <h2>Oneliner</h2>
@@ -15,8 +15,8 @@ export default function About() {
         </div>
 
         <div className={styles.description}>
-         <h2>Discipline and culture</h2>
-         <p>Discipline and learning is the backbone of the development of startup journey & culture is our behavior</p>
+         <h2>Discipline</h2>
+         <p>Discipline and learning is the backbone of the development of runash startup journey</p>
         </div>
 
         <div className={styles.description}>
@@ -32,7 +32,7 @@ export default function About() {
        
        <div className={styles.description}>
        <h2>Executive Profiles</h2>
-       <p>The single most important element of our business model is the management team responsible for making the live retail streaming platform. immediate market-specific knowledge, and experience in implementing the business plans. & changing the nature of retailing & redefine the business mode</p>
+       <p>Management team responsible for making the live retail streaming platform. immediate market-specific knowledge,& experience in implementing the business plans. & changing the nature of retailing & redefine the business mode</p>
        </div>
 
        </div>
