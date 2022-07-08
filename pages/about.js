99a16cd69532279d79960da76edfe2f977d4ed2a
Ram Murmu <runashInc@outlook.com>
2022-07-08 23:58:36 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -55,8 +55,21 @@ export default function About() {
             <a>&larr; Go Back</a>
           </Link>
         </p>
+        <footer className={styles.footer}>
+        <a
+          href="https://runash.in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         © 2022 RunAsh. {' '}
+          <span className={styles.logo}>
+            <Image src="" alt=" " width={72} height={16} />
+          </span>
+        </a>
+     </footer>
+  
+
     </main>
-    </div>
-    
+    </div>   
   )
 }
