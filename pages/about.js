00db473d2ee963f703fead63e6825f7a040d50e4
Ram Murmu <runashInc@outlook.com>
2022-07-09 13:19:08 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ export default function About() {
       <main className={styles.main}>
         <h1>About</h1>
         <div className={styles.description}>{' '}<code className={styles.code}>👏 Welcome to runash</code></div>
-        <div className={styles.title}>Mission</div>
+        <div className={styles.title}><a href="https://">Mission</a></div>
          <p className={styles.paragraph}>Enabling and accessing face to face,self made and real experience to access physical reality</p>
         <div className={styles.grid}>
         <div className={styles.description}>
@@ -62,7 +62,7 @@ export default function About() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         © 2022 RunAsh. {' '}
+         © 2022 RunAsh.All rights reserved. {' '}
           <span className={styles.logo}>
             <Image src="" alt=" " width={72} height={16} />
           </span>
