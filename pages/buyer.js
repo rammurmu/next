3987346b86304a64a7c3d05ecd3b83a6d93853cd
Ram Murmu <runashInc@outlook.com>
2022-07-10 20:10:47 +0530

    Update buyer.js

--- pages/buyer.js
+++ pages/buyer.js
@@ -62,7 +62,7 @@ export default function Buyer() {
         
       </main>
     </div>
-   
+    <div>
       <footer className={styles.footer}>
         <a
           href="https://runash.in"
@@ -75,5 +75,6 @@ export default function Buyer() {
           </span>
         </a>
      </footer>
+     </div>
   )
 }
