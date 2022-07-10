485fa1d42213c27f896c5889954089acac015fc8
Ram Murmu <runashInc@outlook.com>
2022-07-10 20:09:38 +0530

    Update buyer.js

--- pages/buyer.js
+++ pages/buyer.js
@@ -62,5 +62,18 @@ export default function Buyer() {
         
       </main>
     </div>
+   
+      <footer className={styles.footer}>
+        <a
+          href="https://runash.in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         © 2022 RunAsh. {' '}
+          <span className={styles.logo}>
+            <Image src="" alt=" " width={72} height={16} />
+          </span>
+        </a>
+     </footer>
   )
 }
