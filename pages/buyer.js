89353855e4aa1ffd9091047b3a5b10001ad943ea
Ram Murmu <runashInc@outlook.com>
2022-07-10 19:42:06 +0530

    Update buyer.js

--- pages/buyer.js
+++ pages/buyer.js
@@ -1,12 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Buyer.module.css'
 import Link from 'next/link'
 
 export default function Buyer() {
   return (
-    <div className={styles.container}>    
-       <main className={style.main}> 
+    <div className={styles.container}>
+      <main className={styles.main}>
         <h1>Buyer</h1>
         <div className={styles.description}><code className={styles.code}>👏 Welcome to RunAsh</code></div>
         <div className={styles.title}>Watch live <br /><a href="https://">streaming</a></div>
@@ -19,19 +17,17 @@ export default function Buyer() {
         <div className={styles.description}>Live<br /><a href="https://">broadcast</a></div>
           <p>Connect with retailer around the world</p>
         <div className={styles.description}>{' '}<code className={styles.code}><a href="https://">🌀Connect Now</a></code></div>
-        
-        
           <p className={styles.code}>All Categories</p>
-
           <code className={styles.code}>Short by</code>
-
           <div className={styles.description}><code className={styles.code}>🍎Fruit</code>{' '}<code className={styles.code}>🥦Vegetable</code><br />
           <code className={styles.code}>💐Flower</code>{' '}<code className={styles.code}>🥫Grocery</code></div>
           <div className={styles.grid}>
           <card className={styles.card}>
-          <h2>🏪 Stream Store</h2>
-             <p><code className={styles.code}><a href="https://">Watch Now</a></code></p>
+            <h2>🏪 Stream Store</h2>
+              <p><code className={styles.code}><a href="https://">Watch Now</a></code></p>
           </card>
+
+          
           <card className={styles.card}>
             <h2>🏪 Stream store</h2>
               <p><code className={styles.code}><a href="https://">Watch Now</a></code></p>
@@ -52,13 +48,19 @@ export default function Buyer() {
             <h2>🏪 Stream store</h2>
               <p><code className={styles.code}><a href="https://">Watch Now</a></code></p>
           </card>
+          
+
           </div>
-          <p className={styles.paragraph}>
-            <Link href="/">
-              <a>&larr; Go Back</a>
-            </Link>
-         </p>  
+
+
+        <p className={styles.paragraph}>
+          <Link href="/">
+            <a>&larr; Go Back</a>
+          </Link>
+        </p>
+       
+        
       </main>
-     </div> 
+    </div>
   )
 }
