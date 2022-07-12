8b42b45e493c9bfdf0aa1c4eee59ccef8c897df7
Ram Murmu <runashInc@outlook.com>
2022-07-12 22:05:05 +0530

    Update seller.js

--- pages/seller.js
+++ pages/seller.js
@@ -13,129 +13,13 @@ export default function Seller() {
       </Head>
         <main className={styles.main}>
         <h1 className={styles.title}>
-          Makeing<br /><a href="https://runash.live/buyer">business</a><br /><a href="https://runash.live/buyer">experience</a>
+          Make your own business<br /><a href="https://runash.live/buyer">experience</a>
         </h1>   
         <p className={styles.description}>
-          Give your store a real experience on a complete real digitization <br />
-          <code className={styles.code}><a href="https://runash.live/schedule">👩‍💻Make own</a></code>
+          Give your store a real experience on a complete real digitization 
         </p>
         
-        <div className={styles.card}><code className={styles.code}>👥Face2Face</code></div>
-        <div className={styles.grid}>
-          <a href="https://runash.live/seller" className={styles.card}>
-            <h2>🛒Real Selling &rarr;</h2>
-            <p>Where retailers can meet to buyers & demonstrate their products </p>
-          </a>
-
-          <a href="https://runash.live/buyer" className={styles.card}>
-            <h2>🛍️Real Buying &rarr;</h2>
-            <p>Where buyers can meet to sellers & shopping organic products  </p>
-          </a>
-
-          <a
-            href="https://runash.live/remote"
-            className={styles.card}
-          >
-            <h2>🙋Real Chat &rarr;</h2>
-            <p>Follow and get started live video chatting</p>
-          </a>
-
-          <a
-            href="https://runash.live/influencer"
-            className={styles.card}
-          >
-            <h2>🧑‍🤝‍🧑Influencers &rarr;</h2>
-            <p>
-              Live influencers community here you can support businesses 
-            </p>
-          </a>
-
-          <a
-            href="https://runash.live/about"
-            className={styles.card}
-          >
-            <h2>🎓About Us  &rarr;</h2>
-            <p>
-              A live retail streaming hybrid marketplace  for retailing
-            </p>
-          </a>
-
-          <a
-            href="https://runash.live/mobile"
-            className={styles.card}
-          >
-            <h2>📱Mobile App &rarr;</h2>
-            <p>
-              Available for iOS and Android 
-            </p>
-          </a>
-
-          <a
-            href="https://runash.live/video"
-            className={styles.card}
-          >
-            <h2>👩‍💻Remote Jobs &rarr;</h2>
-            <p>
-              Find the best startup jobs curated by runash remote team </p>
-          </a>
-
-          <a
-            href="https://runash.live/streaming"
-            className={styles.card}
-          >
-            <h2>📽️Live stream &rarr;</h2>
-            <p>
-              Schedule a live streaming free 
-            </p>
-          </a>
-
-          <a
-            href="https://runash.live/watched"
-            className={styles.card}
-          >
-            <h2>📺Live watch &rarr;</h2>
-            <p>
-              Watched live on demand  streaming  free
-            </p>
-          </a>
-
-          <a
-            href="https://runash.live/experience"
-            className={styles.card}
-          >
-            <h2>👩‍🎤Real Q & A &rarr;</h2>
-            <p>
-              Ask questions real-time via a live stream chat   
-            </p>
-          </a>
-
-          <a
-            href="https://runash.live/diffserve"
-            className={styles.card}
-          >
-            <h2>📊Votes & Polls &rarr;</h2>
-            <p>
-              Give instant feedback opinion & polling
-            </p>
-          </a>
-
-          <a
-            href="https://runash.live/works"
-            className={styles.card}
-          >
-            <h2>🎙️Audio Stream &rarr;</h2>
-            <p>
-              Schedule a live audio stream free
-            </p>
-          </a>
-        </div>
-
-
-        <div>
-        <p className={styles.description}>
-          Begin your RunAsh{' '}
-          <code className={styles.code}><a href="https://runash.live/oss">journey</a></code>
-        </p>
+      
     
        <a
          href="https://Google.form.com">
@@ -143,19 +27,5 @@ export default function Seller() {
        </a>
       </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://runash.in"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         © 2022 RunAsh. {' '}
-          <span className={styles.logo}>
-            <Image src="" alt=" " width={72} height={16} />
-          </span>
-        </a>
-     </footer>
-  </div>
   )
 }
