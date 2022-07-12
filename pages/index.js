9993ead8fb67a0cf3e8835f02e5506968726f774
Ram Murmu <runashInc@outlook.com>
2022-07-12 18:02:35 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
-import '../components/Navbar.css'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
