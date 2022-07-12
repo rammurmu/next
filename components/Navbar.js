0c4268dd522bc9a14ce69b9e7ad7180f0f44c220
Ram Murmu <runashInc@outlook.com>
2022-07-12 16:23:53 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,30 +3,32 @@ import Link from 'next/link'
 function Navbar(){
   return (
       <nav className='header'>
-      <h1 className='logo'>
-      <a href='#'>RunAsh</a>
-      </h1>
+        <h1 className='logo'>
+          <a href='/home'>RunAsh</a>
+        </h1>
       <ul className={'main-nav'}>
-      <li>
-      <Link href="/home">
-      <a>Home</a>
-      </Link>
-      </li>
-      <li>
-      <Link href="/about">
-      <a>About</a>
-      </Link>
-      </li>
-      <li>
-      <Link href="/seller">
-      <a>Seller</a>
-      </Link>
-      </li>
-      <li>
-      <Link href="/buyer">
-      <a>Buyer</a>
-      </Link>
-      </li>
+       <li>
+         <Link href="/home">
+           <a>Home</a>
+         </Link>
+       </li>
+       <li>
+         <Link href="/about">
+            <a>About</a>
+         </Link>
+       </li>
+       <li>
+         <Link href="/seller">
+           <a>Seller</a>
+         </Link>
+       </li>
+       <li>
+          <Link href="/buyer">
+            <a>Buyer</a>
+          </Link>
+       </li>
+     </ul>
  )
 }
+
 export default Navbar
