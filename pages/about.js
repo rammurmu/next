20c8545f04b6d957985e3548b66831f7c2f9d2e4
Ram Murmu <runashInc@outlook.com>
2022-07-13 12:35:31 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -22,13 +22,25 @@ export default function About() {
          Oneliner
         </h1>   
         <p className={styles.description}>
-           A live streaming hybrid marketplace for retailing
+           A live streaming hybrid marketplace and platform for retailing
         </p>
         <h1 className={styles.title}>
           What we do
         </h1>   
         <p className={styles.description}>
            We create new model of marketplace where retailers face to face demonstrate and presentation their products with buyers
+        </p>
+        <h1 className={styles.title}>
+          What we going to make
+        </h1>   
+        <p className={styles.description}>
+           We are committed to building a live streaming platform for unorganized retailers and new ways to use physical reality experience
+        </p>
+        <h1 className={styles.title}>
+          Executive profile
+        </h1>   
+        <p className={styles.description}>
+           Board of Directors<be />management team responsible for making the live streaming platform
         </p>
       </main>
       </div>
