871a081764661668d3995a18bd21a3e6a67004d6
Ram Murmu <runashInc@outlook.com>
2022-07-14 18:37:27 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
+import { Provider } from 'next-auth/client'
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import '../components/Navbar.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider session={pageProps.session}>
       <Navbar />
       <Component {...pageProps} />   
-    </>
+    </Provider>
  )
 }
 
