bed8ba911cb690d3d30a6f65612daf0bd45c8c9b
Ram Murmu <runashInc@outlook.com>
2022-07-15 14:54:45 +0530

    Update index.js

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,8 @@
-function Blog() {
-  return <h1>Blog</h1>
-}
+import Link from 'next/link';
 
-export default Blog
+<h1 className="title">
+  Read{' '}
+  <Link href="/posts/first-post">
+    <a>this page!</a>
+  </Link>
+</h1>
