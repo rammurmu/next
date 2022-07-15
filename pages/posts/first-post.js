e5ae73eb2e3c1332510da79c2481a2c0115ce079
Ram Murmu <runashInc@outlook.com>
2022-07-15 19:28:47 +0530

    Update first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import styles from '../styles/Blog.module.css'
 
 export default function FirstPost() {
   return (
     <>
-      <h1>Blog</h1>
-      <p>The latest news about RunAsh from the RunAsh team</p>
+      <h1 className={styles.title}>Introduction</h1>
+      <p>I would like to introduce runash,runash is a live streaming hybrid marketplace and platform for retailing</p>
       <hr />
 
       <h2>
