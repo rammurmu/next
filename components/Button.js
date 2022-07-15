a85f0480c62b848625ae7c7d259df138c237a178
Ram Murmu <runashInc@outlook.com>
2022-07-15 13:04:27 +0530

    Create button.js

---
+++ components/Button.js
@@ -0,0 +1,12 @@
+import styles from './Button.module.css'
+
+export function Button() {
+  return (
+    <button
+      type="button"
+      className={styles.button}
+    >
+      Learn
+    </button>
+  )
+}
