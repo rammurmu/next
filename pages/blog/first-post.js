9b17d8e23d326a76e47f452b781f352a868fc5ae
Ram Murmu <runashInc@outlook.com>
2022-07-15 15:49:50 +0530

    Update and rename first.js to first-post.js

---
+++ pages/blog/first-post.js
@@ -0,0 +1,5 @@
+function FirstPost() {
+  return <h1>First Post</h1>
+}
+
+export default First Post
--- pages/blog/first.js
+++
@@ -1,5 +0,0 @@
-function FirstBlog() {
-  return <h1>First blog Page</h1>
-}
-
-export default FirstBlog
