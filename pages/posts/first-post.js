67f48b0b10d6454f06230718e9e87f16b5a046dd
Ram Murmu <runashInc@outlook.com>
2022-07-15 16:15:46 +0530

    Update first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,27 @@
+import Link from 'next/link';
+
 export default function FirstPost() {
-  return <h1>First Post</h1>;
+  return (
+    <>
+      <h1>Blog</h1>
+      <p>The latest news about RunAsh from the RunAsh team< br />Hey, I'm a Senior Software Engineer at RunAsh. I enjoy working with Next.js and crafting beautiful front-end experiences.<br />This portfolio is built with **Next.js** and a library called [Nextra](https://nextra.vercel.app/). It allows you to write Markdown and focus on the _content_ of your portfolio.</p>
+      <hr />
+
+
+
+
+
+
+
+
+
+      <h2>
+        <Link href="/">
+          <a>&larr; Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
 }
+
+
