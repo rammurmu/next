26bd2ad8bc639879c5d0b8ca977cf5b0e8080cd2
Ram Murmu <runashInc@outlook.com>
2022-07-18 19:40:44 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
       <nav className='header'>
         <h1 className='logo'>
-          <a href='/index'>RunAsh.live</a>
+          <a href='/index'>RunAsh</a>
         </h1>
       <ul className={'main-nav'}>
        <li>
