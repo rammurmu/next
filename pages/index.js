c8f3bf0f39db97dcbeb35d32a659666c6f9e88e2
Ram Murmu <runashInc@outlook.com>
2022-07-21 13:07:50 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -132,12 +132,13 @@ export default function Home() {
             </p>
           </a>
         </div>
+      <div>
         <form onSubmit={handleSubmit}>
         <label htmlFor="email">email</label>
         <input type="email" id="email" pattern="@global\.com" size="30" required>
         <button type="sign up">Sign Up</button>
       </form>
-
+      </div>
         <div>
       </div>
       </main>
