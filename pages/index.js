24ee3606ee9fbab86d71c0f80012df3379e19be0
Ram Murmu <runashInc@outlook.com>
2022-07-21 21:24:40 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.icon" />  
       </Head>
         <main className={styles.main}>
-        <p className={styles.description}><code className={styles.code}><b>🆕 Get latest update</b> Learn more &rarr;</code></p>
+        <p className={styles.description}><code className={styles.code}><b>🆕Latest update</b> Learn more &rarr;</code></p>
         <h1 className={styles.title}>
          Live retail streaming <br /><a href="https://">platform</a>
          </h1>
@@ -22,7 +22,7 @@ export default function Home() {
          </p>
          
          <button type="button-secondary">Signin</button>
-         <p>Get start 🆓 &rarr;</p>
+         <p>Get started free &rarr;</p>
 
         <div className={styles.grid}>
           <a href="https://runash.live/seller" className={styles.card}>
