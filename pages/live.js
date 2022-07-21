024f0cb4ae0b8acdae762d8d24b7b69a6b4772b0
Ram Murmu <runashInc@outlook.com>
2022-07-21 21:58:51 +0530

    Create live.js

---
+++ pages/live.js
@@ -0,0 +1,17 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Live() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Live shopping streaming</h1>
+        <p className={styles.description}>Schedule a live shopping</p>
+          <Link href="/">
+            <a>&larr; Go Back</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  )
+}
