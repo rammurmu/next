c195d5792fc8c7dc857027e1cc551f531bc147a3
Ram Murmu <runashInc@outlook.com>
2022-07-23 20:53:28 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -28,15 +28,15 @@ export default function About() {
          </div>
          <p className={styles.description}>Discipline and learning</p>
         <div className={styles.card}>
-        <h2>👥<br />What we do </h2>
+        <p>👥<br /><b>What we do</b></p>
         <p>We created new model of live streaming marketplace and environment where retailers face to face demonstrate and presented their products with buyers<br />Learn more &rarr;</p>
         </div>
         <p className={ styles.description}>Founders story</p>
         <div className={styles.card}>
-        <h1>🎥<br />What we going to make</h1>
+        <p>🎥<br /><b>What we going to make</b></p>
         <p>We are committed to building a live retail streaming hybrid platform for unorganised retailers and new ways to use physical reality experience<br />Learn more &rarr;</p>
         </div>
-         <h1 className={styles.description}>Executive profile</h1>
+         <p className={styles.description}>Executive profile</p>
           <p className={styles.description}>Board of Directors & Management team</p>
         <p className={styles.description}>
           <Link href="/">
