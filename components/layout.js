52a7365f3e0bea8dfa912435e736a6eb9cd8b3f9
Ram Murmu <runashInc@outlook.com>
2022-07-24 16:17:45 +0530

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,13 @@
 import Link from 'next/link'
+import Header from './header'
 import Navbar from './navbar'
 import Footer from './footer'
-import Header from './header'
+
 
 export default function Layout({ children }) {
   return (
     <>
+      <Header />
       <Navbar />
       <main>{children}</main>
       <Footer />
