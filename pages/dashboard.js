ee29336252a88981be0b37c10eff611e6fbc76c3
Ram Murmu <runashInc@outlook.com>
2022-07-24 08:31:00 +0530

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,8 @@
-import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Dashboard.module.css'
 import Link from 'next/link'
+
 
 export default function Dashboard() {
   return (
