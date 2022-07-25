44ee209b46828aeb0567f60d17a91348eb2c1b07
Ram Murmu <runashInc@outlook.com>
2022-07-25 13:27:01 +0530

    Update seller.js

--- pages/seller.js
+++ pages/seller.js
@@ -12,6 +12,7 @@ export default function Seller() {
         <link rel="icon" href="/favicon.icon" />  
       </Head>
         <main className={styles.main}>
+        <p className={styles.description}><code className={styles.code}>👏 Welcome to RunAsh</code></p>
         <h1 className={styles.title}>
           Make your own made<br /><a href="https://">business</a>
         </h1>   
@@ -63,6 +64,7 @@ export default function Seller() {
          </div>
          <br />
          <p>Shaping the future of retail</p>
+         <p className={styles.card}>Create your business on aadhaar &rarr;</p>
       </main>
       </div>
   )
