e8ef9c8717cb56e9a6ee44785784153336a636a6
Ram Murmu <runashInc@outlook.com>
2022-07-26 09:55:45 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import '../components/Navbar.css'
+import variables from '../styles/variables.module.scss'
 
 
 function MyApp({ Component, pageProps }){
@@ -8,11 +9,14 @@ function MyApp({ Component, pageProps }){
   return (
     <>  
       <Navbar />
+     <Layout color={variables.primaryColor}>
       <Component {...pageProps} />  
+     </Layout>
     </>
  )
 }
 
 export default MyApp
 
-
+  
+ 
