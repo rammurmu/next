561b019c74dc35f228ed443fbf0df073fa1856f0
Ram Murmu <runashInc@outlook.com>
2022-07-26 10:02:34 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,6 @@ function MyApp({ Component, pageProps }){
   return (
     <>  
      <Layout color={variables.primaryColor}>
-      <Navbar />
       <Component {...pageProps} />  
      </Layout>
     </>
