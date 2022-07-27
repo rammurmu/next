9c47e7aaf83d9ce010711d0fe4dd54f6c9971377
Ram Murmu <runashInc@outlook.com>
2022-07-27 13:16:48 +0530

    Update buyer.js

--- pages/buyer.js
+++ pages/buyer.js
@@ -21,6 +21,52 @@ export default function Buyer() {
         </p>
         <button type="Watch live ">Watch live</button>
        <h2>Categories</h2>
+      <>
+       <nav className='header'>
+          <Link href='/dashboard'>
+          <a>RunAsh</a>
+          </Link>
+        </h1>
+      <ul className={'main-nav'}>
+       <li>
+         <Link href='/about'>
+            <a>Startup</a>
+         </Link>
+       </li>
+       <li>
+         <Link href='/seller'>
+           <a>Seller</a>
+         </Link>
+       </li>
+       <li>
+          <Link href='/buyer'>
+            <a>Buyer</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/blog'>
+            <a>Blog</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/js-form'>
+            <a>Signin</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/live'>
+            <a>Live</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/productlist'>
+            <a>Login</a>
+          </Link>
+       </li>     
+        
+     </ul>
+   </nav>
+  </>
       <div className={styles.grid}>
           <a href="https://productslist" className={styles.card}>
             <h2>🍎🍋Fruit</h2>
