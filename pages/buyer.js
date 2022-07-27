9f5c8c327f4f89a35f5636083ddcf0fa8312e932
Ram Murmu <runashInc@outlook.com>
2022-07-27 12:23:30 +0530

    Update buyer.js

--- pages/buyer.js
+++ pages/buyer.js
@@ -76,10 +76,7 @@ export default function Buyer() {
     <h1 className={styles.description}>Watched live on demand</h1>
     <div className={styles.box}>
     <p>Schedule a live shopping</p>
-   <form>
-   <div className={styles.lable}>select</div>
-   <select className={styles.select}>select</div>
-   </form>  
+ 
     </div>
      </main> 
     </div> 
