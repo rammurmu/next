1ca3fe6050386a673d56ff68c9867741cd8cba74
Ram Murmu <runashInc@outlook.com>
2022-07-30 11:34:46 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,18 @@ export default function Layout({ children }) {
     <>
       <Header />
       <main>{children}</main>
-      <Footer />
+      <footer className={styles.footer}>
+        <a
+          href="https://runash.in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         © 2022 RunAsh. {' '}
+          <span className={styles.logo}>
+            <Image src="" alt=" " width={72} height={16} />
+          </span>
+        </a>
+     </footer>
     </>
   )
 }
