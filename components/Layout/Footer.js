4027e37e67847741c5bc233dd0d031c6fe12512c
Ram Murmu <runashInc@outlook.com>
2022-07-30 13:54:05 +0530

    Update Footer.js

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,19 +1,7 @@
-import Footer from '../components/Footer.css'
-import Link from 'next/link'
-
-export default function Footer() {
-  return (
-    <div className={styles.footer}>
-      <main className={styles.main}>
-        <h1>footer</h1>
-        <p className={styles.description}>
-          <Link href="/">
-            <a>Learn</a>
-          </Link>
-        </p>
-      </main>
-    </div>
-  )
+function Footer() {
+  return <div className='footer'>Footer</div>
 }
+
+export default Footer
 
 
