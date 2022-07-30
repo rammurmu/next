ee2b287b654e890c1857947da859d457cbb22fc4
Ram Murmu <runashInc@outlook.com>
2022-07-30 14:28:17 +0530

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -2,17 +2,13 @@ import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Blog.module.css'
 
-function Blog({ title, description  }) {
+function Blog() {
   return (
     <>
       <Head>
         <title>Blog</title>
         <meta name='description' content={description} />
       </Head>
-      <h1 className='content'>
-         {process.env.DB_USER} Password {process.env.DB_PASSWORD}{' '} 
-         {process.env.NEXT_PUBLIC_ANALYTICS_ID} 
-      </h1>
       <p className={styles.description}><code className={styles.code}>👏 Welcome to RunAsh</code></p>
       <h1 className={styles.title}>
       Live Blog
@@ -20,7 +16,7 @@ function Blog({ title, description  }) {
       <p className={styles.description}>The latest guides,and  announcement</p>
       
        
-       <p className={styles.p}><b>Introduction</b>Introduceing the next generation and future of eCommerce <br />Live streaming for buyers <br />Live streaming for sellers <br />Watched live-on-demand <br />Live video chat<br /> Live influencers community<br /> Live Q and A or poll</p>
+       <p className={styles.p}><b>Introduction</b><br />Introduceing the next generation and future of eCommerce <br />Live streaming for buyers <br />Live streaming for sellers <br />Watched live-on-demand <br />Live video chat<br /> Live influencers community<br /> Live Q and A or poll</p>
        
        <Link href="/posts/first-post">
        <a>Read More &rarr;</a>
@@ -38,16 +34,3 @@ function Blog({ title, description  }) {
 
 export default Blog
 
-export async function getServerSideProps() {
-  const user = process.env.DB_USER
-  const password = process.env.DB_PASSWORD
-  console.log(
-    `Connecting to database with username ${user} and password ${password}`
-  )
-  return {
-    props: {
-      title: 'Article Title',
-      description: 'Article description'
-    }
-  }
-}
