e95f804f4613bcbf8e31909017eead93899078f8
Ram Murmu <runashInc@outlook.com>
2022-07-30 13:35:09 +0530

    Update Footer.js

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -2,6 +2,18 @@ import Link from 'Next/link'
 
 function Footer() {
   return <div className='footer'>Footer</div>
+        <footer className={styles.footer}>
+        <a
+          href="https://runash.in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         © 2022 RunAsh. {' '}
+          <span className={styles.logo}>
+            <Image src="" alt=" " width={72} height={16} />
+          </span>
+        </a>
+     </footer>
 }
 
 export default Footer
