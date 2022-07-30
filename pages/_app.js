99d5ccf86f531b5ff230461ddad63d3d2a794adc
Ram Murmu <runashInc@outlook.com>
2022-07-30 12:49:14 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import '../components/Navbar.css'
-
-
+import 'styles/layout.css'
+import Footer from '@/layout/Footer'
 
 
 function MyApp({ Component, pageProps }){
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />)
+  }
   return (
     <> 
      <Navbar />
       <Component {...pageProps} />  
+      <Footer />
     </>
  )
 }
