c8d51cb3ea43ebbfc716a07ff6357b92cd875c87
Ram Murmu <runashInc@outlook.com>
2022-07-30 12:00:33 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,19 +2,6 @@ import Link from 'next/link'
 
 function Footer() {
   return (
-         
-        <footer className='footer'>
-        <a
-          href="https://runash.in"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         © 2022 RunAsh. {' '}
-          <span className={styles.logo}>
-            <Image src="" alt=" " width={72} height={16} />
-          </span>
-        </a>
-     </footer>
      
  )
 }
