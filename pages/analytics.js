1202e3bda95cda7ba39513db190d4143e57d3754
Ram Murmu <runashInc@outlook.com>
2022-08-01 00:01:13 +0530

    Create analytics.js

---
+++ pages/analytics.js
@@ -0,0 +1,20 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Analytics() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+       <div className={styles.card}>
+        <h1>Analytics</h1>
+        <p>Live insight</p>
+        </div>
+         <p className={styles.description}>
+          <Link href="/">
+            <a>&larr; Back to home</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  )
+}
