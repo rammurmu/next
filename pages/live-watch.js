2db9e03ea9b0d3b2366fc7af0aa53e82f169706b
Ram Murmu <runashInc@outlook.com>
2022-08-01 00:22:00 +0530

    Create live-watch.js

---
+++ pages/live-watch.js
@@ -0,0 +1,20 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Live watch() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+       <div className={styles.card}>
+        <h1>Live watch</h1>
+        <p>watched live on demand </p>
+        </div>
+         <p className={styles.description}>
+          <Link href="/">
+            <a>&larr; Back to home</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  )
+}
