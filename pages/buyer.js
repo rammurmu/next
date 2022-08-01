2c2882932a8da2985e940e8e22b66d34f37a7eaf
Ram Murmu <runashInc@outlook.com>
2022-08-01 12:46:21 +0530

    Update buyer.js

--- pages/buyer.js
+++ pages/buyer.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import Sidebar from '../components/sidebar'
+import Sidebar from '../components/Sidebar'
 
 export default function Buyer() {
   return (
