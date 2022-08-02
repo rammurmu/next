4929daa904cd0c2f5183e9d4b2c30b03896c29c0
Ram Murmu <runashInc@outlook.com>
2022-08-02 14:14:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.icon" />  
       </Head>
         <main className={styles.main}>
-        <card className={styles.card}"placeholder 🆕 Latest update for live reatil streaming &rarr;" />
+        <input className={styles.input}"placeholder 🆕 Latest update for live reatil streaming &rarr;" />
       
         <h1 className={styles.title}>
          Live retail streaming <br /><a href="https://">platform</a>
