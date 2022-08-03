c481522ae6acf78e4f56020bab735410866c0695
Ram Murmu <runashInc@outlook.com>
2022-08-03 12:48:53 +0530

    Update Menu.js

--- components/Menu.js
+++ components/Menu.js
@@ -1,31 +1,9 @@
 import Link from 'next/link'
 import styles from 'styles/mobile-menu.module.css';
 
-export default function MobileMenu() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { mounted: isMenuMounted, rendered: isMenuRendered } = useDelayedRender(
-    isMenuOpen,
-    {
-      enterDelay: 20,
-      exitDelay: 300
-    }
-  );
+function Menu() {
+  return <div className='menu'>Menu</div>
+}
 
-  function toggleMenu() {
-    if (isMenuOpen) {
-      setIsMenuOpen(false);
-      document.body.style.overflow = '';
-    } else {
-      setIsMenuOpen(true);
-      document.body.style.overflow = 'hidden';
-    }
-  }
-
-  useEffect(() => {
-    return function cleanup() {
-      document.body.style.overflow = '';
-    };
-  }, []);
-
-
+export default Menu
 
