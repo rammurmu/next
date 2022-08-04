79a1399b62e67ccbf25bfae119ae63f50ae29068
Ram Murmu <runashInc@outlook.com>
2022-08-04 15:31:04 +0530

    Create remote.js

---
+++ pages/remote.js
@@ -0,0 +1,20 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Remote() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+       <div className={styles.title}>
+        <h1>Remote jobs</h1>
+        <p>Best startup jobs at runash Connect with remote team founders can reach you via email</p>
+        </div>
+         <p className={styles.description}>
+          <Link href="/">
+            <a>&larr; Back to home</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  )
+}
