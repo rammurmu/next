30026bd19f72cd441bb22e531d3d61d465ea6c72
Ram Murmu <runashInc@outlook.com>
2022-08-04 15:21:27 +0530

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,20 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Contact() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+       <div className={styles.title}>
+        <h1>Contact Us</h1>
+        <p>Reach and connect with us </p>
+        </div>
+         <p className={styles.description}>
+          <Link href="/">
+            <a>&larr; Back to home</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  )
+}
