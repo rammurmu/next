413e03e1a46372a8cbea2c923f85c5d24456b02d
Ram Murmu <runashInc@outlook.com>
2022-08-04 19:58:00 +0530

    Update faq.js

--- pages/faq.js
+++ pages/faq.js
@@ -10,11 +10,11 @@ export default function Faq() {
         <p>Some questions are commonly started on runash</p>
         </div>
 
-        <= className={styles.detailsMenu}select id="select" name="select">
+        <Menu className={styles.detailsMenu}select id="select" name="select">
         <option><a href="https://">Select</a></option>
         <option>Selling</option>
         <option>Shopping</option>
-        </=>
+        </Menu>
        
          <details>
             <summary>We make something people want</summary>
