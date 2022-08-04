ae9ae39df048e0a41ee9ddba53c1c218ff79ed2e
Ram Murmu <runashInc@outlook.com>
2022-08-04 15:10:21 +0530

    Create faq.js

---
+++ pages/faq.js
@@ -0,0 +1,20 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Faq() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+       <div className={styles.title}>
+        <h1>Frequently asked questions</h1>
+        <p>Some frequently asked questions are commonly started in runash.in</p>
+        </div>
+         <p className={styles.description}>
+          <Link href="/">
+            <a>&larr; Back to home</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  )
+}
