28c535c115107f302cae7ed091414afca2974838
Ram Murmu <runashInc@outlook.com>
2022-08-05 16:13:33 +0530

    Update help.js

--- pages/help.js
+++ pages/help.js
@@ -6,32 +6,43 @@ export default function Help() {
     <div className={styles.container}>
       <main className={styles.main}>
        <div className={styles.title}>
-        <h1>Help team</h1>
-        <p>Help for wherever you are on your runash journey.</p>
+        <h1>Helpful culture</h1>
+        <p className={styles.description}>Help for wherever you are on your runash journey.</p>
 
-       <h1>Choose a topic to help us route your request quickly</h1>
-        
-       <div className={styles.card}>👤 Account Issues</div>
-
-       <div className={styles.card}>🪙 Billing Issues</div>
-
-       <div className={styles.card}>🧺 Cart Issues</div>
-
-       <div className={styles.card}>🛍️ Shopping Issues</div>
-
-       <div className={styles.card}>🛒 Selling Issues</div>
-
-       <div className={styles.card}>📽️ Live Streaming Issues</div>
-
-       <div className={styles.card}>📺Watch live streaming Issues</div> 
-
-       <div className={styles.card}>📝 Provide Feedback</div>
-
-       <div className={styles.card}>🤷 Help with runash </div>
-
-       <div className={styles.card}>👫 Influencer Issues</div> 
-
-       <div className={styles.card}>⚙️ Other support</div>
+       <h1>Choose a topic to support your request quickly</h1>
+       <div className={styles.card}>
+       <p className={styles.description}>👤 Account Issues</p>
+       </div>
+       <div className={styles.card}>
+        <p className={styles.description}>🪙 Billing Issues</p>
+       </div>
+       <div className={styles.card}>
+        <p className={styles.description}>🧺 Cart Issues</p>
+        </div>
+       <div className={styles.card}>
+        <p className={styles.description}>🛍️ Shopping Issues</p>
+        </div>
+       <div className={styles.card}>
+        <p className={styles.description}>🛒 Selling Issues</p>
+       </div>
+       <div className={styles.card}>
+        <p className={styles.description}>📽️ Live Streaming Issues</p>
+       </div>
+       <div className={styles.card}>
+       <p className={styles.description}>📺Watch live streaming Issues</p> 
+       </div>
+       <div className={styles.card}>
+       <p className={styles.description}>📝 Provide Feedback</p>
+       </div>
+       <div className={styles.card}>
+       <p className={styles.description}>🤷 Help with runash </p>
+       </div>
+       <div className={styles.card}>
+        <p className={styles.description}>👫 Influencer Issues</p> 
+       </div>
+       <div className={styles.card}>
+        <p className={styles.description}>⚙️ Other support</p>
+       </div>
 
        <p className={styles.description}>Connect and interaction with CEO on live streaming </p> 
         </div>
