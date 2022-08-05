c1715cb62e2e0e58821884488e14520d10579997
Ram Murmu <runashInc@outlook.com>
2022-08-05 17:55:12 +0530

    Update remote.js

--- pages/remote.js
+++ pages/remote.js
@@ -9,6 +9,11 @@ export default function Remote() {
         Remote jobs and internships</h1>
         <p className={styles.description}>Best startup jobs at runash Connect with remote team founders can reach you via email</p>
         
+
+       <p className={styles.description}><b>Where runash can hire</b>We love and believe our remote first culture at runash and we currently have hiring entities in all the following states, and countries</p>
+
+       
+
          <p className={styles.description}>
           <Link href="/">
             <a>&larr; Back to home</a>
