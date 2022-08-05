dc6479cef911d4d9f7b7e931d641472967ab64aa
Ram Murmu <runashInc@outlook.com>
2022-08-05 17:28:31 +0530

    Update remote.js

--- pages/remote.js
+++ pages/remote.js
@@ -15,8 +15,8 @@ export default function Remote() {
          <h1 className={style.title}>
           Where we can hire
          </h1>
-         <p className={styles.description}>We love and believe our remote first culture at runash and we currently have hiring entities in all the following states, and countries.</p>
-         
+         <p className={styles.description}>We love and believe our remote first culture at runash and we currently have hiring entities in all the following states, and countries</p>
+        
       </main>
     </div>
   )
