a1fbc33d7017cafca121bb3697092dc513078239
Ram Murmu <runashInc@outlook.com>
2022-08-06 19:58:03 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       </Head>
         <main className={styles.main}>
         
-        <div className={styles.cards}><b><code className={styles.code}>New</code>Updates:</b> Introducing the next generation and future of live-retail streaming &rarr;</div>
+        <div className={styles.cards}><code className={styles.code}>New</code><b>Updates:</b> Introducing the next generation and future of live-retail streaming &rarr;</div>
         
         <h1 className={styles.title}>
          Live retail streaming <br /><a href="https://">platform</a>
