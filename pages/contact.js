a2480a404bad27713b0be142e667411a2d7bcb8c
Ram Murmu <runashInc@outlook.com>
2022-08-06 19:01:34 +0530

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -7,8 +7,9 @@ export default function Contact() {
       <main className={styles.main}>
        <div className={styles.title}>
         <h1>Contact Us</h1>
-        <p>Reach and connect with us </p>
+        <p>Reach anytime and connect with us </p>
         </div>
+       <div className={styles.code}>New</code>
          <p className={styles.description}>
           <Link href="/">
             <a>&larr; Back to home</a>
