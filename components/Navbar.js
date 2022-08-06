0949ee56b4c82b629da53e4a150e5a8f966ffda7
Ram Murmu <runashInc@outlook.com>
2022-08-06 17:26:17 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import Sidebar from '../components/Sidebar'
 
 function Navbar() {
   return (
-      <header className={'header'}>
+      <header className={'header'}>welcome to runash</header>
       <nav className={'skip-nav'}>
       <ul className={'main-nav'}>
       <h1 className='logo'>
