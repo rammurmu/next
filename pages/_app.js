7cd23dd2407325a7f4910491f405ae45f324035f
Ram Murmu <runashInc@outlook.com>
2022-08-06 17:44:50 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,11 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import '../components/Navbar.css'
-import Header from '../components/Headers'
-import '../components/Header.css'
 
 function MyApp({ Component, pageProps }){
   return (
     <> 
      <Navbar />
-      <Component {...pageProps} />  
-    </>
-    <> 
-     <Header />
       <Component {...pageProps} />  
     </>
  )
