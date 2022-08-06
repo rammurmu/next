1fc94c0ca298142d92d7a92539ae97755daa3370
Ram Murmu <runashInc@outlook.com>
2022-08-06 23:04:30 +0530

    Update feedback.js

--- pages/feedback.js
+++ pages/feedback.js
@@ -7,7 +7,7 @@ export default function Feedback() {
       <main className={styles.main}>
        <h1 className={styles.title}>
         Feedback</h1>
-        <p className={styles.description}>Give your most valuable feedback and make runash better for you</p>
+        <p className={styles.description}>Your most valuable feedback make runash better for you</p>
         
 
         <p>Share your runash feedback<br />How satisfied are you with runash live streaming</p>
@@ -15,6 +15,10 @@ export default function Feedback() {
         <input className={styles.checkbox} checkbox id='checkbox' value='Very satisfied' />
         <input className={styles.button} button id='button' vale='very satisfied' />
         <input className={styles.time} time id='appt' name='appt' min='09:00' max='18:00' required />
+        <input className={style.time} time id='time' value='Schedule' />
+          <value>Go live</value>
+          <value>Booking</valu>
+         </input>
          <p className={styles.description}>
           <Link href="/">
             <a>&larr; Back to home</a>
