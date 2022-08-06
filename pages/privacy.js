28ba4ff34e0f0e3454acf03642aeace4aed1781e
Ram Murmu <runashInc@outlook.com>
2022-08-06 18:00:41 +0530

    Create privacy.js

---
+++ pages/privacy.js
@@ -0,0 +1,22 @@
+import styles from '../styles/Blog.module.css'
+import Link from 'next/link'
+
+export default function Privacy() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+       <div className={styles.title}>
+        <h1>Privacy policy</h1>
+        <p class name={styles.p}>
+
+        </p>
+        </div>
+         <p className={styles.description}>
+          <Link href="/">
+            <a>&larr; Back to home</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  )
+}
