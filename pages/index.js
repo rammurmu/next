542afd48262829c2d94ed2f93615c2d7d450990d
Ram Murmu <runashInc@outlook.com>
2022-08-07 21:47:41 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -141,8 +141,8 @@ export default function Home() {
        <div className={styles.cards}><code className={styles.codes}>👍 Give your most valuable feedback ⭐ &rarr;</code></div>
       
       <div className={styles.cards}>
-      <label className={styles.label}><h2>Sign up weekly updates for live streaming</h2></label>
-      <input className={styles.input} placeholder="email address..." pattern="https://runash.in" size="20" required /><buttons type="Subscribe">Subscribe</buttons>
+      <label className={styles.label}>Sign up weekly updates for live retail streaming</label>
+      <input className={styles.input} placeholder="email address..." pattern="https://runash.in" size="20" required /><button type="button">Subscribe</button>
       </div>
       <div>
       </div>
