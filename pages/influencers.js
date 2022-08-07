31f9e6a4ce1a3d989d48eb3dd0c203be6d56e278
Ram Murmu <runashInc@outlook.com>
2022-08-07 23:45:45 +0530

    Update influencers.js

--- pages/influencers.js
+++ pages/influencers.js
@@ -6,8 +6,8 @@ export default function Influencers() {
     <div className={styles.container}>
       <main className={styles.main}>
        <div className={styles.title}>
-        <h1>Live streaming influencers community</h1>
-        <p>INFLUENCER PROGRAM<br />The role of runash influencers<br /> If products and services are being bought and sold online,you can automatically (and correctly)assume that such products and services are also marketed online.This is where influencers come in .Influencers are trusted have loyal followers, and are highly entertaining.It is often why brands will have brand ambassadors representing them.Acelebrity or an influencer will have the power to take the brand and the products to the next level with their extension reach Lear more</p>
+        <h1 className={styles.title}>Live influencers community</h1>
+        <p className={styles.description}>Influencers program if organic products and services are being bought and sold live streaming,you can automatically (and correctly)assume that such organic products and services are also marketed live</p>
         </div>
          <p className={styles.description}>
           <Link href="/">
