ca8124b8fc0357affdfea1406c4ee558ab64412c
Ram Murmu <runashInc@outlook.com>
2022-08-08 17:33:33 +0530

    Update feedback.js

--- pages/feedback.js
+++ pages/feedback.js
@@ -39,12 +39,12 @@ export default function PageWithJSbasedFormFeedback() {
     <div className={styles.card1}>
       <h1 className={styles.title}>
         <Link href="/">
-          <a>RunAsh</a>
+          <a>Feedback</a>
         </Link>{' '}
       </h1>
       
       <p className={styles.description}>
-        Create your account
+        Your feedback make our products better 
       </p>
 
       <form onSubmit={handleSubmit}>
@@ -59,6 +59,30 @@ export default function PageWithJSbasedFormFeedback() {
 
         <button type="sign up">Sign Up</button>
       </form>
+
+     
+       <h1 className={styles.title}>
+        Feedback</h1>
+        <p className={styles.description}>Your most valuable feedback make runash better for you</p>
+        
+
+        <p>Share your runash feedback<br />How satisfied are you with runash live streaming</p>
+        <input className={styles.radio} radio id='choice' value='Very satisfied' />
+        <input className={styles.checkbox} checkbox id='checkbox' value='Very satisfied' />
+        <input className={styles.button} button id='button' vale='very satisfied' />
+        <input className={styles.time} time id='appt' name='appt' min='09:00' max='18:00' value='required' />
+       
+          
+         
+         <p className={styles.description}>
+          <Link href="/">
+            <a>&larr; Back to home</a>
+          </Link>
+        </p>
+      
+
+
+
       
       <p className={styles.description}>
          &rarr;
