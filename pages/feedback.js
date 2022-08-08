171c30171fca53502e07ed9f9e38dda431be6c40
Ram Murmu <runashInc@outlook.com>
2022-08-08 17:29:56 +0530

    Update feedback.js

--- pages/feedback.js
+++ pages/feedback.js
@@ -1,29 +1,70 @@
-import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import styles from '../styles/Home.module.css'
 
-export default function Feedback() {
+export default function PageWithJSbasedFormFeedback() {
+  // Handle the submit event on form submit.
+  const handleSubmit = async (event) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
+
+    // Get data from the form.
+    const data = {
+      first: event.target.first.value,
+      last: event.target.last.value,
+    }
+
+    const JSONdata = JSON.stringify(data)
+
+    // Send the form data to our API and get a response.
+    const response = await fetch('/api/form', {
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+
+      // Tell the server we're sending JSON.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // The method is POST because we are sending data.
+      method: 'POST',
+    })
+
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json()
+    alert(`Is this your full name: ${result.data}`)
+  }
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-       <h1 className={styles.title}>
-        Feedback</h1>
-        <p className={styles.description}>Your most valuable feedback make runash better for you</p>
-        
+    <main className={styles.main}>
+    <div className={styles.card1}>
+      <h1 className={styles.title}>
+        <Link href="/">
+          <a>RunAsh</a>
+        </Link>{' '}
+      </h1>
+      
+      <p className={styles.description}>
+        Create your account
+      </p>
 
-        <p>Share your runash feedback<br />How satisfied are you with runash live streaming</p>
-        <input className={styles.radio} radio id='choice' value='Very satisfied' />
-        <input className={styles.checkbox} checkbox id='checkbox' value='Very satisfied' />
-        <input className={styles.button} button id='button' vale='very satisfied' />
-        <input className={styles.time} time id='appt' name='appt' min='09:00' max='18:00' value='required' />
-       
-          
-         
-         <p className={styles.description}>
-          <Link href="/">
-            <a>&larr; Back to home</a>
-          </Link>
-        </p>
-      </main>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="first">First Name</label>
+        <input type="text" id="first" name="first" required />
+        <label htmlFor="last">Last Name</label>
+        <input type="text" id="last" name="last" required />
+        <label htmlFor="Phone number">Phone number</label>
+        <input type="text" id="Phone number" name="Optional" required />
+        <label htmlFor="password">Password</label>
+        <input type="text" id="password" name="password" required />
+
+        <button type="sign up">Sign Up</button>
+      </form>
+      
+      <p className={styles.description}>
+         &rarr;
+      </p>
+     </div>
+     </main>
     </div>
   )
 }
