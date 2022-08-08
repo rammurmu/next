d3b1e16ce4435af00408543f2ab038768c20b5ee
Ram Murmu <runashInc@outlook.com>
2022-08-08 23:00:41 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -22,53 +22,32 @@ export default function About() {
         <p className={styles.description}>
           We create real-life experiences to enable and access face-to-face
         </p>
-        <p className={styles.description}>Startup journey ✨
-        <br />
-         Journey from a small village in India to Sillicone Valley Y Combinator startup school community member<br />
-         <br />
-         Learn more &rarr;</p>
+        <h2 className={styles.title}>Startup journey ✨</h2>
+         <p className={styles.description}>Journey from a small village in India to Sillicone Valley Y Combinator startup school community member<br />Learn more &rarr;</p>
 
-        <p className={styles.description}>🤔 Oneliner<br />A live streaming hybrid marketplace for retailing<br />
-        <br />
-          Learn more &rarr;</p>
-        <br />
+        <h2 className={styles.description}>🤔 Oneliner</h2>
+          <p className={styles.description}>A live streaming hybrid marketplace for retailing<br />Learn more &rarr;</p>
         
-         <p className={styles.description}>🎓 Discipline,learning & culture<br />
-          Discipline and learning is the bacbone of the development of our startup journy and culture is behavior<br />
-          <br />
-           Learn more &rarr;</p>
-        <br />
-        <p className={styles.description}>👥 What we do<br />We created new model of live streaming marketplace and environment where retailers face to face demonstrate and presented their products with buyers<br />
-       <br />
-       Learn more &rarr;</p>
-       <br />
-        <p className={styles.description}>🎥 What we going to make<br />We are committed to building a live retail streaming hybrid platform for unorganised retailers and new ways to use physical reality experience<br />
-        <br />
-         Learn more &rarr;</p>
-         <br />
-         <p className={ styles.description}>🎤Founders story 
-         <br />🤼 We are both brothers,our business journey started in 2007 from a small offline retail store, the Journey from the school classroom to the board room
-         <br />
-          Learn more &rarr;</p>
+        
+         <h2 className={styles.description}>🎓 Discipline,learning & culture</h2>
+          <p className={styles.description}>Discipline and learning is the bacbone of the development of our startup journy and culture is behavior<br />Learn more &rarr;</p>
+        
+        <h2 className={styles.description}>👥 What we do</h2>
+         <p className={styles.description}>We created new model of live streaming marketplace and environment where retailers face to face demonstrate and presented their products with buyers<br />Learn more &rarr;</p>
+       
+        <h2 className={styles.description}>🎥 What we going to make</h2>
+          <p className={styles.description}>We are committed to building a live retail streaming hybrid platform for unorganised retailers and new ways to use physical reality experience<br />Learn more &rarr;</p>
+         
+         <h2 className={ styles.description}>🎤Founders story </h2>
+         <p className={styles.description}>🤼 We are both brothers,our business journey started in 2007 from a small offline retail store, the Journey from the school classroom to the board room<br >Learn more &rarr;</p>
 
-         <p className={styles.description}>👨‍💻 Executive profile<br />Board of Directors<br />
-         <ul>
-         <li>Ram Murmu</li>
-         <li>Vaibhav Murmu</li>
-         </ul>
-          <br />
-          Learn more &rarr;</p>
-         <br />
-         <div>
+         <h1 className={styles.description}>👨‍💻 Executive profile</h2>
+          <P className={styles.description}>Board of Directors<br />Ram Murmu andVaibhav Murmu<br /> Learn more &rarr;</p>
+         
+         
          <h2 className={styles.title}>Backed by</h2>
          <p className={styles.description}>Open Source</p>
-         </div>
-         <br />
-        <p className={styles.description}>
-          <Link href="/">
-            <a><button type="button" className={styles.button}>&larr; Back</button></a>
-          </Link>
-         </p>
+         
       </main>
      <div>
        <ul className={'main-nav'}>
