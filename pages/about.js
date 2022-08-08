5c1b2e56cb00871015614707e99650327acfaab2
Ram Murmu <runashInc@outlook.com>
2022-08-08 16:57:19 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -120,7 +120,7 @@ export default function About() {
         </Link>
        </li>  
        <li>
-         <link href='/form'>
+         <Link href='/form'>
          <a>Form</a>
        </Link>
        </li> 
