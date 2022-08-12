3a8e12b4729feed521165e70170d98d9f6ce5fba
Ram Murmu <runashInc@outlook.com>
2022-08-12 12:43:52 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,13 +4,14 @@ import Sidebar from '../components/Sidebar'
 
 function Navbar() {
   return (
+      <nav className={'skip-nav'}>
       <ul className={'main-nav'}>
       <h1 className='logo'>
           <Link href='/dashboard'>
           <a>RunAsh</a>
           </Link>
         </h1>
-        <li>
+       <li>
          <Link href='/about' scroll={true}>
             <a>Startup</a>
          </Link>
@@ -36,12 +37,12 @@ function Navbar() {
           </Link>
        </li> 
        <li>
-       <Link href='/influencers' scroll={true}>
+       <Link href="/influencers" scroll={true}>
          <a>Influencers</a>
       </Link>
       </li>       
       <li>
-       <Link href='/store' scroll={true}>
+       <Link href="/store" scroll={true}>
          <a>Store</a>
       </Link>
       </li>               
@@ -51,62 +52,62 @@ function Navbar() {
           </Link>
        </li> 
        <li>
-       <Link href='/cart' scroll={true}>
-         <a>Cart</a>
+       <Link href="/cart" scroll={true}>
+         <a>🛒</a>
       </Link>
       </li>            
       <li>
-       <Link href='/setting' scroll={true}>
-         <a>Setting</a>
+       <Link href="/setting" scroll={true}>
+         <a><b>⚙️</b></a>
       </Link>
       </li> 
       <li>
-        <Link href='/about'> scroll={true}>
+        <Link href='/about'>
          <a>About</a>
          </Link>
        </li>
        <li>
-         <Link href='/privacy'> scroll={true}>
+         <Link href='/privacy'>
            <a>Privacy</a>
          </Link>
        </li>
        <li>
-          <Link href='/help'> scroll={true}>
+          <Link href='/help'>
             <a>Help</a>
           </Link>
        </li>     
        <li>
-          <Link href='/remote'> scroll={true}>
+          <Link href='/remote'>
             <a>Remote</a>
           </Link>
        </li>     
        <li>
-          <Link href='/schedule'> scroll={true}>
+          <Link href='/schedule'>
             <a>Schedule</a>
           </Link>
        </li>     
        <li>
-          <Link href='/feedback'> scroll={true}>
+          <Link href='/feedback'>
             <a>Feedback</a>
           </Link>
        </li>     
        <li>
-          <Link href='/faq'> scroll={true}>
+          <Link href='/faq'>
             <a>FAQ</a>
           </Link>
        </li> 
        <li>
-          <Link href='/resource'> scroll={true}>
+          <Link href='/resource'>
             <a>Resource</a>
           </Link>
        </li>     
        <li>
-          <Link href='/docs'> scroll={true}>
+          <Link href='/docs'>
             <a>Docs</a>
           </Link>
        </li> 
        <li>
-          <Link href='/blog'> scroll={true}>
+          <Link href='/blog'>
             <a>Blog</a>
           </Link>
        </li>                 
