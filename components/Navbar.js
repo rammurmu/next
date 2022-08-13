0ce365ae5fb955dee9de0cb5d809047cfc700a66
Ram Murmu <runashInc@outlook.com>
2022-08-13 18:55:41 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -50,67 +50,7 @@ function Navbar() {
           <Link href='/js-form'>
             <a>LogIn</a>
           </Link>
-       </li> 
-       <li>
-       <Link href="/cart">
-         <a>Cart</a>
-      </Link>
-      </li>            
-      <li>
-       <Link href="/setting">
-         <a>Setting</a>
-      </Link>
-      </li> 
-      <li>
-        <Link href='/about'> 
-         <a>About</a>
-         </Link>
-       </li>
-       <li>
-         <Link href='/privacy'> 
-           <a>Privacy</a>
-         </Link>
-       </li>
-       <li>
-          <Link href='/help'> 
-            <a>Help</a>
-          </Link>
-       </li>     
-       <li>
-          <Link href='/remote'> 
-            <a>Remote</a>
-          </Link>
-       </li>     
-       <li>
-          <Link href='/schedule'> 
-            <a>Schedule</a>
-          </Link>
-       </li>     
-       <li>
-          <Link href='/feedback'> 
-            <a>Feedback</a>
-          </Link>
-       </li>     
-       <li>
-          <Link href='/faq'> 
-            <a>FAQ</a>
-          </Link>
-       </li> 
-       <li>
-          <Link href='/resource'> 
-            <a>Resource</a>
-          </Link>
-       </li>     
-       <li>
-          <Link href='/docs'> 
-            <a>Docs</a>
-          </Link>
-       </li> 
-       <li>
-          <Link href='/blog'> 
-            <a>Blog</a>
-          </Link>
-       </li>                 
+       </li>        
      </ul>  
    </nav>  
  )
