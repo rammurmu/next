522538810b24a48f7a0e23eac5089ba5ca1be5b7
Ram Murmu <runashInc@outlook.com>
2022-08-18 15:29:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,4 +5,27 @@ const nextConfig = {
 
 module.exports = nextConfig
 
-
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'X-About-Custom-Header',
+            value: 'about_header_value',
+          },
+        ],
+      },
+      {
+        source: '/seller/:id',
+        headers: [
+          {
+            key: 'X-Seller-Custom-Header',
+            value: 'seller_header_value',
+          },
+        ],
+      },
+    ]
+  },
+}
