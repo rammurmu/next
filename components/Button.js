4bf409c0934cdd639aa9ceabfe8223d375494c50
Ram Murmu <runashInc@outlook.com>
2022-08-18 14:56:41 +0530

    Update Button.js

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,6 @@
-import Link from 'next/link'
 import styles from './Button.module.css'
 
 export default function Button(props) {
-  return (
-    <button
-      type="button"
-      className={styles.button} {...props} />
+  return <button type="button" className={styles.btn} {...props} />
 }
 
