4bbb91461c094d2d869f37886565bf1ceca4e587
Ram Murmu <runashInc@outlook.com>
2022-08-18 13:35:11 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,9 +7,9 @@ function Navbar() {
       <nav className={'header'}>
        <h1 className='logo'>
          <Link href='/index'>
-         <a>
+         <span>
             <Image src="/vercel.svg" alt=" " width={72} height={16} />
-         </a>
+         </span>
          </Link>
        </h1>
        
