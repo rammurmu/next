2b4850229926671b16ebc6492eefd6b259762ee8
Ram Murmu <runashInc@outlook.com>
2022-08-21 17:15:43 +0530

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -194,19 +194,5 @@ export default function Blog() {
       </ul>
     </div>
     </div>
-    <div>
-    <footer className={styles.footer}>
-        <a
-          href="https://runash.in"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         © 2022 RunAsh. {' '}
-          <span className={styles.logo}>
-            <Image src="" alt=" " width={72} height={16} />
-          </span>
-        </a>
-     </footer>
-    </div>
   )
 }
