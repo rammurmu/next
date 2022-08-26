3f1b99a94ece0819c448a3fb6ff424d0e351f511
Ram Murmu <runashInc@outlook.com>
2022-08-26 18:14:14 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -15,13 +15,13 @@ export default function About() {
         <link rel="icon" href="favicon.icon"/>
       </Head>
       <main className={styles.main}> 
-       <h1>Where the retailers build real experince</h1> 
+       <h1 className={styles.title}>Where the retailers build real experince</h1> 
       
       <p className={styles.description}>
         <a href="https://">Our mission</a>
       </p>
         <h1 className={styles.title}>
-          We create real-life experiences to enable retailers and access face-to-face
+          We create real life experiences to enable retailers and access face to face
         </h1>
         <p className={styles.description}>We build live streaming products for <b>retailer and businesses.</b><br />And those who aspire to become one.</p>
         <div className={styles.grid}>
