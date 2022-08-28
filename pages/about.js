a8dbe6c09a72ecb70732c4a76e903ba465a5f867
Ram Murmu <runashInc@outlook.com>
2022-08-28 16:00:08 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -64,7 +64,7 @@ export default function About() {
         
        
     
-         <div className={styles.card}>
+        
          <h2>
           Join us and shape the future of retail
           <h2>
