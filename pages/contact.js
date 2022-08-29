266c94500dcfa9b4aa16a86343d644d4055e40cc
Ram Murmu <runashInc@outlook.com>
2022-08-29 23:01:56 +0530

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -3,7 +3,38 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 
-export default function Contact() {
+export default function PageWithJSbasedFormContact() {
+  // Handle the submit event on form submit.
+  const handleSubmit = async (event) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
+
+    // Get data from the form.
+    const data = {
+      first: event.target.first.value,
+      last: event.target.last.value,
+    }
+
+    const JSONdata = JSON.stringify(data)
+
+    // Send the form data to our API and get a response.
+    const response = await fetch('/api/form', {
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+
+      // Tell the server we're sending JSON.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // The method is POST because we are sending data.
+      method: 'POST',
+    })
+
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json()
+    alert(`Is this your full name: ${result.data}`)
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -13,10 +44,24 @@ export default function Contact() {
       </Head>
       <main className={styles.main}>
        <div className={styles.title}>
-        <h1>Reach to a RunAsh team</h1>
-        <p>Fill free to contact and connect  with us for  </p>
+        <h1 className={styles.title}>Reach to a RunAsh team</h1>
+        <p className={styles.description}>We personaly meet  to your business and pricing needs,so please contact and connect  with us   </p>
         </div>
-       
+
+      <div className={styles.card}>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="first">First Name</label>
+        <input type="text" id="first" name="first" required />
+        <label htmlFor="last">Last Name</label>
+        <input type="text" id="last" name="last" required />
+        <label htmlFor="Phone number">Phone number</label>
+        <input type="text" id="Phone number" name="Optional" required />
+        <label htmlFor="password">Password</label>
+        <input type="text" id="password" name="password" required />
+
+        <button type="submit">Submit</button>
+      </form>
+        
 
 
          
