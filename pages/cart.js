b7149f930ce64dc0a00410d1d404776f83d2ac1a
Ram Murmu <runashInc@outlook.com>
2022-08-30 00:11:53 +0530

    Update cart.js

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,7 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Cart() {
   return (
@@ -9,12 +11,79 @@ export default function Cart() {
         <h1>Shopping cart</h1>
         <p>Personalized your cart <br />Your cart is empty</p>
         </div>
-         <p className={styles.description}>
-          <Link href="/">
-            <a>&larr; Back to home</a>
-          </Link>
-        </p>
+       
       </main>
+      <div>
+       <ul className={'main-nav'}>
+       <li>
+         <Link href='/about'>
+            <a>About</a>
+         </Link>
+       </li>
+       <li>
+         <Link href='/privacy'>
+            <a>Privacy</a>
+         </Link>
+       </li>
+       <li>
+         <Link href='/help'>
+           <a>Help</a>
+         </Link>
+       </li>
+       <li>
+          <Link href='/contact'>
+            <a>Contact</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/feedback'>
+            <a>Feedback</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/faq'>
+            <a>FAQ</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/careers'>
+            <a>Careers</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/blog'>
+            <a>Blog</a>
+          </Link>
+       </li>  
+       <li>
+         <Link href='/media'>
+          <a>Media</a>
+        </Link>
+       </li>  
+       <li>
+         <Link href='/api'>
+         <a>API</a>
+       </Link>
+       </li> 
+       <li>
+        <Link href='/status'>
+        <a>Status</a>
+        </Link>
+      </li>
+      </ul>
+  </div>
+   <footer className={styles.footer}>
+        <a
+          href="https://runash.in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         © 2022 RunAsh. {' '}
+          <span className={styles.logo}>
+            <Image src="" alt=" " width={72} height={16} />
+          </span>
+        </a>
+     </footer>
     </div>
   )
 }
