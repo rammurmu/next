7ca4f5b59f05500881b6c4bab9090c9dc9912333
Ram Murmu <runashInc@outlook.com>
2022-08-29 23:36:35 +0530

    Update influencers.js

--- pages/influencers.js
+++ pages/influencers.js
@@ -1,20 +1,98 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Head from 'next/Head'
+import Image from 'next/image'
 
 export default function Influencers() {
   return (
     <div className={styles.container}>
+      <Head> 
+        <title>Support</title>
+        <meta name="description" content="Live influencers support "/>
+        <link rel="icon" href="favicon.icon"/>
+      </Head>
       <main className={styles.main}>
        <div className={styles.title}>
-        <h1 className={styles.title}>Influencers community</h1>
-        <p className={styles.description}>Influencers program if organic products and services are being bought and sold live streaming,you can automatically assume that such organic products and services are also marketed live</p>
+        <h1 className={styles.title}>Live Influencers</h1>
+        <p className={styles.description}>Live influencers community is a helpfulness support community for retail businesses</p>
         </div>
-         <p className={styles.description}>
-          <Link href="/">
-            <a>&larr; Back to home</a>
-          </Link>
-        </p>
+         
+
+
+
+
       </main>
+       <div>
+       <ul className={'main-nav'}>
+       <li>
+         <Link href='/about'>
+            <a>About</a>
+         </Link>
+       </li>
+       <li>
+         <Link href='/privacy'>
+            <a>Privacy</a>
+         </Link>
+       </li>
+       <li>
+         <Link href='/help'>
+           <a>Help</a>
+         </Link>
+       </li>
+       <li>
+          <Link href='/contact'>
+            <a>Contact</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/feedback'>
+            <a>Feedback</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/faq'>
+            <a>FAQ</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/careers'>
+            <a>Careers</a>
+          </Link>
+       </li>     
+       <li>
+          <Link href='/blog'>
+            <a>Blog</a>
+          </Link>
+       </li>  
+       <li>
+         <Link href='/media'>
+          <a>Media</a>
+        </Link>
+       </li>  
+       <li>
+         <Link href='/api'>
+         <a>API</a>
+       </Link>
+       </li> 
+       <li>
+        <Link href='/status'>
+        <a>Status</a>
+        </Link>
+      </li>
+      </ul>
+    </div>
+    <footer className={styles.footer}>
+        <a
+          href="https://runash.in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         © 2022 RunAsh. {' '}
+          <span className={styles.logo}>
+            <Image src="" alt=" " width={72} height={16} />
+          </span>
+        </a>
+     </footer>
     </div>
   )
 }
