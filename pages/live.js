3a13a990496d9b62bdc6f0f25c7cd8e70da5f7ad
Ram Murmu <runashInc@outlook.com>
2022-08-29 20:09:01 +0530

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -105,5 +105,17 @@ export default function Live() {
       </ul>
     </div>
    </div>
+   <footer className={styles.footer}>
+        <a
+          href="https://runash.in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         © 2022 RunAsh. {' '}
+          <span className={styles.logo}>
+            <Image src="" alt=" " width={72} height={16} />
+          </span>
+        </a>
+     </footer>
   )
 }
